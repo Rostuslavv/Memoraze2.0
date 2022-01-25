8514aa99b66bd0e11c4e0be80abc2fc3d3164d4c
Rostuslavv <rostuslavdz@gmail.com>
2022-01-25 11:38:13 +0200

    Initial Commit

---
+++ Memoraze2.0/Memoraze2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Memoraze2_0App.swift
+//  Memoraze2.0
+//
+//  Created by Rostyslav on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Memoraze2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
